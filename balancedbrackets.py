3e0f891ef31512df4860f79ed458db8784cea9aa
Rachel Suk <rachelsuk97@gmail.com>
2021-07-19 11:51:48 -0700

    completed balancedbrackets

---
+++ balancedbrackets.py
@@ -0,0 +1,77 @@
+"""Does a given string have balanced pairs of brackets?
+
+Given a string, return True or False depending on whether the string
+contains balanced (), {}, [], and/or <>.
+
+Many of the same test cases from Balance Parens apply to the expanded
+problem, with the caveat that they must check all types of brackets.
+
+These are fine::
+
+   >>> has_balanced_brackets("<ok>")
+   True
+
+   >>> has_balanced_brackets("<{ok}>")
+   True
+
+   >>> has_balanced_brackets("<[{(yay)}]>")
+   True
+
+These are invalid, since they have too many open brackets::
+
+   >>> has_balanced_brackets("(Oops!){")
+   False
+
+   >>> has_balanced_brackets("{[[This has too many open square brackets.]}")
+   False
+
+These are invalid, as they close brackets that weren't open::
+
+   >>> has_balanced_brackets(">")
+   False
+
+   >>> has_balanced_brackets("(This has {too many} ) closers. )")
+   False
+
+Here's a case where the number of brackets opened matches
+the number closed, but in the wrong order::
+
+    >>> has_balanced_brackets("<{Not Ok>}")
+    False
+
+If you receive a string with no brackets, consider it balanced::
+
+   >>> has_balanced_brackets("No brackets here!")
+   True
+
+"""
+
+
+def has_balanced_brackets(phrase):
+    """Does a given string have balanced pairs of brackets?
+
+    Given a string as input, return True or False depending on whether the
+    string contains balanced (), {}, [], and/or <>.
+    """
+    bracket_stack = []
+    bracket_dict = {
+        "(": ")",
+        "{": "}",
+        "[": "]",
+        "<": ">"
+    }
+    for char in phrase:
+        if char in bracket_dict:
+            bracket_stack.append(char)
+        elif char in bracket_dict.values():
+            if not bracket_stack or (bracket_dict[bracket_stack.pop()] != char):
+                return False
+    if not bracket_stack:
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU CAUGHT ALL THE STRAY BRACKETS!\n")
