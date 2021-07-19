22391a04ee3969fb5e62841b277a9f69b19c3703
Rachel Suk <rachelsuk97@gmail.com>
2021-07-19 15:56:36 -0700

    completed boggle

---
+++ boggle.py
@@ -0,0 +1,144 @@
+"""Boggle word check.
+
+Given a 5x5 boggle board, see if you can find a given word in it.
+
+In Boggle, you can start with any letter, then move in any NEWS direction.
+You can continue to change directions, but you cannot use the exact same
+tile twice.
+
+So, for example::
+
+    N C A N E
+    O U I O P
+    Z Q Z O N
+    F A D P L
+    E D E A Z
+
+In this grid, you could find `NOON* (start at the `N` in the top
+row, head south, and turn east in the third row). You cannot find
+the word `CANON` --- while you can find `CANO` by starting at the
+top-left `C`, you can 't re-use the exact same `N` tile on the
+front row, and there's no other `N` you can reach.
+
+For example::
+
+    >>> board = make_board('''
+    ... N C A N E
+    ... O U I O P
+    ... Z Q Z O N
+    ... F A D P L
+    ... E D E A Z
+    ... ''')
+
+`NOON` should be found (0, 3) -> (1, 3) -> (2, 3) -> (2, 4)::
+
+    >>> find(board, "NOON")
+    True
+
+`NOPE` should be found (0, 3) -> (1, 3) -> (1, 4) -> (0, 4)::
+
+    >>> find(board, "NOPE")
+    True
+
+`CANON` can't be found (`CANO` starts at (0, 1) but can't find
+the last `N` and can't re-use the N)::
+
+    >>> find(board, "CANON")
+    False
+
+You cannot travel diagonally in one move, which would be required
+to find `QUINE`::
+
+    >>> find(board, "QUINE")
+    False
+
+We can recover if we start going down a false path (start 3, 0)::
+
+    >>> find(board, "FADED")
+    True
+
+
+An extra tricky case --- it needs to find the `N` toward the top right,
+and then go down, left, up, up, right to find all four `O`s and the `S`::
+
+    >>> board2 = make_board('''
+    ... E D O S Z
+    ... N S O N R
+    ... O U O O P
+    ... Z Q Z O R
+    ... F A D P L
+    ... ''')
+
+    >>> find(board2, "NOOOOS")
+    True
+
+"""
+
+
+def make_board(board_string):
+    """Make a board from a string.
+
+    For example::
+
+        >>> board = make_board('''
+        ... N C A N E
+        ... O U I O P
+        ... Z Q Z O N
+        ... F A D P L
+        ... E D E A Z
+        ... ''')
+
+        >>> len(board)
+        5
+
+        >>> board[0]
+        ['N', 'C', 'A', 'N', 'E']
+    """
+
+    letters = board_string.split()
+
+    board = [
+        letters[0:5],
+        letters[5:10],
+        letters[10:15],
+        letters[15:20],
+        letters[20:25],
+    ]
+
+    return board
+
+
+def find(board, word):
+    """Can word be found in board?"""
+    index = 0
+    coords_to_check = []
+    coords_used = []
+    for i in range(5):
+        for j in range(5):
+            coords_to_check.append([word[0], i, j])
+    while coords_to_check:
+        coord = coords_to_check.pop()
+        letter = coord[0]
+        row = coord[1]
+        col = coord[2]
+        if board[row][col] == letter:
+            if index == (len(word) - 1):
+                return True
+            coords_used.append([letter, row, col])
+            index += 1
+            letter = word[index]
+            if row > 0 and ([letter, row-1, col] not in coords_used):
+                coords_to_check.append([letter, row-1, col])
+            if row < len(board)-1 and ([letter, row+1, col] not in coords_used):
+                coords_to_check.append([letter, row+1, col])
+            if col > 0 and ([letter, row, col-1] not in coords_used):
+                coords_to_check.append([letter, row, col-1])
+            if col < len(board)-1 and ([letter, row, col+1] not in coords_used):
+                coords_to_check.append([letter, row, col+1])
+    return False
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED; YOU FOUND SUCCESS! ***\n")
